bd3c555ae33c147775c60642be8e56e0f0f5e207
peeratchai Matanboon <icekungz42200@gmail.com>
2025-08-03 21:44:41 +0700

    Create SurgeonFish.java

---
+++ SurgeonFish.java
@@ -0,0 +1,11 @@
+public class SurgeonFish extends Fish {
+    @Override
+    public void swim() {
+        System.out.println("SurgeonFish is swimming gracefully!");
+    }
+}public class SurgeonFish extends Fish {
+    @Override
+    public void swim() {
+        System.out.println("SurgeonFish is swimming gracefully!");
+    }
+}
